54be4ceb2966e68e2a5e9bff5b656f6ccf3ec29a
ricbon35 <ricbon35@gmail.com>
2017-11-17 11:43:30 +0100

    correctif

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -1,18 +1,21 @@
 package java8.ex02;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
 import java8.data.Data;
 import java8.data.domain.Customer;
 import java8.data.domain.Order;
 import java8.data.domain.Pizza;
-import org.junit.Test;
-
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 /**
  * Exercice 02 - Transformation
@@ -20,33 +23,31 @@ import static org.junit.Assert.*;
 public class Stream_02_Test {
 
 	@Test
-    public void test_map() throws Exception {
+	public void test_map() throws Exception {
 
-        List<Order> orders = new Data().getOrders();
+		List<Order> orders = new Data().getOrders();
 
-        // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = orders.stream()
-        		.map(o -> o.getCustomer())
-        		.distinct()
-        		.collect(Collectors.toList());
+		// Trouver la liste des clients ayant déjà passés une commande
+		List<Customer> result = orders.stream()
+								.map(o -> o.getCustomer()).distinct().collect(Collectors.toList());
 
-        assertThat(result, hasSize(2));
-    }
+		assertThat(result, hasSize(2));
+	}
 
-    @Test
-    public void test_flatmap() throws Exception {
+	@Test
+	public void test_flatmap() throws Exception {
 
-        List<Order> orders = new Data().getOrders();
+		List<Order> orders = new Data().getOrders();
 
-        // TODO calculer les statistiques sur les prix des pizzas vendues
-        // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = orders.stream()
-        		.flatMap(o -> o.getPizzas().stream())
-        		.collect(Collectors.summarizingInt(Pizza::getPrice));
-        
-        assertThat(result.getSum(), is(10900L));
-        assertThat(result.getMin(), is(1000));
-        assertThat(result.getMax(), is(1375));
-        assertThat(result.getCount(), is(9L));
-    }
+		// TODO calculer les statistiques sur les prix des pizzas vendues
+		// TODO utiliser l'opération summaryStatistics
+		IntSummaryStatistics result = orders.stream()
+									.flatMap(o -> o.getPizzas().stream())
+									.collect(Collectors.summarizingInt(p->p.getPrice()));
+		
+		assertThat(result.getSum(), is(10900L));
+		assertThat(result.getMin(), is(1000));
+		assertThat(result.getMax(), is(1375));
+		assertThat(result.getCount(), is(9L));
+	}
 }
